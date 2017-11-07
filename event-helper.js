7a37bee3c64022f18c6d396b7bae08833209e5cf
chiiaik <chiiaikfang@gmail.com>
2017-11-07 09:45:57 +0800

    Initial commit

---
+++ event-helper.js
@@ -0,0 +1,43 @@
+function EventHelper(event) {
+    this.event = event;
+    this.getBody = function() {
+        if (!this.event.body) {
+            return null;
+        }
+        try {
+            return JSON.parse(this.event.body);
+        } catch (error) {
+
+        }
+        return null;
+    };
+
+    this.getApiKey = function() {
+        if (!this.event.requestContext ||
+            !this.event.requestContext.identity ||
+            !this.event.requestContext.identity.apiKey) {
+            return null;
+        }
+        try {
+            return JSON.parse(this.event.requestContext.identity.apiKey);
+        } catch (error) {
+
+        }
+        return null;
+    };
+
+    this.getQueryStrings = function() {
+        if (!this.event.queryStringParameters) {
+            return null;
+        }
+        return this.event.queryStringParameters;
+    };
+
+    this.getPathParameters = function() {
+        if (!this.event.pathParameters) {
+            return null;
+        }
+        return this.event.pathParameters;
+    };
+}
+module.exports = EventHelper;
